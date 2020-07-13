ee62a5f7330d3f249a16bb985ddca7ee0b5a7c80
ym <mengyve.d@gmail.com>
2020-07-13 14:39:21 +0800

    add all

---
+++ encyclopedia/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+
+# Create your views here.
